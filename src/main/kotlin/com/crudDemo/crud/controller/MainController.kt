b87fbf016ac63edd7c717782d9cb25c9b5be45db
Mazbah <mazbah142@gmail.com>
2021-05-12 14:11:56 +0600

    Another one

--- src/main/kotlin/com/crudDemo/crud/controller/MainController.kt
+++ src/main/kotlin/com/crudDemo/crud/controller/MainController.kt
@@ -3,10 +3,9 @@ package com.crudDemo.crud.controller
 import com.crudDemo.crud.model.Todo
 import com.crudDemo.crud.repository.TodoRepo
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.ControllerAdvice
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.*
+import javax.print.attribute.IntegerSyntax
 
 @Controller
 @ControllerAdvice
@@ -20,6 +19,33 @@ class MainController(private var pr: TodoRepo) {
     @RequestMapping("/addTodo")
     fun addTodo(@ModelAttribute todo: Todo): String {
         pr.save(todo)
+        return "redirect:/home"
+    }
+
+    @RequestMapping("/findById")
+    fun findById(@RequestParam id:Integer, m: Model):String{
+        var t:Todo = pr.getOne(id)
+        println(t.id)
+        if(t.id !=null) {
+            m.addAttribute("todo",t)
+             return "TodoInfo.html"
+        }else return "No data found by this ID"
+    }
+
+    @RequestMapping("/deleteTodo")
+    fun deleteTodo(@RequestParam id:Integer):String{
+        pr.deleteById(id)
+        return "redirect:/home"
+    }
+
+    @RequestMapping("/updateTodo")
+    fun updateTodo(@ModelAttribute todo: Todo):String{
+        var t:Todo = pr.getOne(todo.id!!)
+        t.title = todo.title
+        t.Des = todo.Des
+        pr.save(t)
         return "TodoInfo.html"
     }
+
+
 }
--- src/main/kotlin/com/crudDemo/crud/model/Todo.kt
+++ src/main/kotlin/com/crudDemo/crud/model/Todo.kt
@@ -6,11 +6,12 @@ import javax.persistence.*
 @Table(name="todo_list")
 data class Todo(
     @Id
-    @Column(name="id") @GeneratedValue(strategy = GenerationType.IDENTITY)  val id: Integer? = null,
-    val title: String,
-    val Des: String
+    @Column(name="id") @GeneratedValue(strategy = GenerationType.IDENTITY)  var id: Integer? = null,
+    var title: String,
+    @Column(name="Description") var Des: String
 ) {
     override fun toString(): String {
         return "Todo [Id = $id Title = $title Des = $Des ]"
     }
+
 }
